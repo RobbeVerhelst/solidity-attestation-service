61f43b8dd232d6308e83f4b55703c04f327ec924
Robbe Verhelst <robbe.verhelst@gmail.com>
2024-06-10 11:18:25 +0200

    feat: update sol version

--- contracts/CustomEIP712Proxy.sol
+++ contracts/CustomEIP712Proxy.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
+pragma solidity 0.8.26;
 
 import { EIP712Proxy } from "eas-contracts/eip712/proxy/EIP712Proxy.sol";
 import { CustomEAS } from "./CustomEAS.sol";
